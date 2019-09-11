798cba541ac5ff59731f36d4912cbb1d3e7e55b1
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2019-09-11 14:24:14 +0200

    Add a python file to be used as a template to quickly test new features

---
+++ exec/test_feature.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import baggianalysis as ba
+import numpy as np
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage is %s topology configuration" % sys.argv[0], file=sys.stderr)
+    exit(0)
+    
+parser = ba.GenericOxDNAParser(sys.argv[1])
+trajectory = ba.FullTrajectory(parser)
+trajectory.add_filter(ba.FilterByFunction(lambda x: x.position[0] < 0))
+trajectory.initialise_from_trajectory_file(sys.argv[2])
+
+syst = trajectory.next_frame()
+print(syst.N())
