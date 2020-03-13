abe49b731f035912acdbb35293efadd3cd1d86fa
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2020-03-13 14:26:05 +0100

    Improve the look-and-feel of the sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 from recommonmark.transform import AutoStructify
 
+import sphinx_rtd_theme
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -35,12 +37,14 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
-    'recommonmark'
+    'recommonmark',
+    "sphinx_rtd_theme"
 ]
 
 napoleon_include_init_with_doc = True
 autosummary_generate = True
 autoclass_content = 'both'
+autodoc_default_flags = ["show-inheritance", ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -106,7 +110,7 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -122,12 +126,14 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pyramid'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation' : False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
