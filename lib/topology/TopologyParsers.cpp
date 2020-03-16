c4b98b2c027ea96316bc5a4bff9db9047ca20f75
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2020-03-16 18:03:20 +0100

    Bugfix to the parse_microgel_bondfile function

--- lib/topology/TopologyParsers.cpp
+++ lib/topology/TopologyParsers.cpp
@@ -35,17 +35,19 @@ void parse_microgel_bondfile(std::string filename, std::shared_ptr<Topology> top
 			int p_idx = utils::lexical_cast<int>(split[0]) - 1;
 			uint n_neighs = utils::lexical_cast<uint>(split[1]);
 
-			// the second line contains the indexes of all neighbours
-			std::getline(input, line);
-			split = utils::split(utils::trim(line));
-			if(split.size() != n_neighs) {
-				std::string error = boost::str(boost::format("Particle %d seems to have %u neighbours, should be %u") % p_idx % split.size() % n_neighs);
-				throw std::runtime_error(error);
-			}
+			if(n_neighs > 0) {
+				// the second line contains the indexes of all neighbours
+				std::getline(input, line);
+				split = utils::split(utils::trim(line));
+				if(split.size() != n_neighs) {
+					std::string error = boost::str(boost::format("Particle %d seems to have %u neighbours, should be %u") % p_idx % split.size() % n_neighs);
+					throw std::runtime_error(error);
+				}
 
-			for(uint i = 0; i < split.size(); i++) {
-				int q_idx = utils::lexical_cast<int>(split[i]) - 1;
-				topology->add_bond(p_idx, q_idx);
+				for(uint i = 0; i < split.size(); i++) {
+					int q_idx = utils::lexical_cast<int>(split[i]) - 1;
+					topology->add_bond(p_idx, q_idx);
+				}
 			}
 		}
 	}
