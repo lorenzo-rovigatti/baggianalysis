4b44761fe8055c4f9558d7593f8ec2136d7f3521
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2021-03-05 09:15:05 +0100

    Update the docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,10 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.3a1.dev5+gbbf749d.d20210301',
+    VERSION: '0.0.3a1.dev6+g8591eec',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: false
 };
