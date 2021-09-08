cc61417ea4e51e9fd2ca6d2389a299af42099c96
CI workflow <github-actions@github.com>
2021-09-08 19:57:42 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0b1.dev4+gf604419',
+    VERSION: '0.3.2.dev3+g69cf27f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
